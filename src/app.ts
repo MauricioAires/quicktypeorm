6d5c91e0bb9a36b9d57c9809e21d9cd9f7f8ac8d
MauricioAires <mauricioaires015@gmail.com>
2023-07-25 15:41:17 -0300

    feat(middleware) handle express errors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
-import express from "express";
-import routes from "./routes";
+import express from 'express';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'express-async-errors';
+import routes from './routes';
+import handleAppErrors from './middleware/errors';
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use(handleAppErrors);
 
 export default app;
